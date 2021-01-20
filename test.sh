988c73849ccba9a6661ec59986da42cdd93cf936
gogo9th <gogo9th@hanmail.net>
2021-01-20 23:08:05 +0100

    added testcases and testcase program

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+answers=(
+100
+370
+280
+270
+375
+50
+30
+20
+15
+130
+45
+30
+95
+0
+)
+for (( i = 1; i <= 14; i++)); do
+	echo "<Test $i>"
+	./sku test$i.txt
+	echo "Expected Total Price: ${answers[$((i - 1))]}"
+	echo
+done
